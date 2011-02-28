b7f2614c74c1c77cd3295227fa7de8c55f6d58ab
Miguel Madero <curtingit@miguelmadero.com>
2011-02-28 13:03:53 +0800

    .

--- Src/Commons/Persistence/IBaseEntity.cs
+++ Src/Commons/Persistence/IBaseEntity.cs
@@ -2,6 +2,11 @@
 
 namespace BoC.Persistence
 {
+    public interface IBaseEntity<out TKey> : IBaseEntity
+    {
+        new TKey Id { get; }
+    }
+
     public interface IBaseEntity : IEquatable<IBaseEntity>
     {
         object Id { get; }
