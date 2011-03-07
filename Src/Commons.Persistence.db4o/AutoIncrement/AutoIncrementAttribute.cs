9a8c446a2bd34059db1e172d98e21f2501f440eb
pworst <pworst@gmail.com>
2011-03-07 13:15:32 +0100

    Added default autoincrement support for db4o.

---
+++ Src/Commons.Persistence.db4o/AutoIncrement/AutoIncrementAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Commons.Persistence.db4o.AutoIncrement
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class AutoIncrementAttribute : Attribute
+    {
+    }
+}
