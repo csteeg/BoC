9bb23bb1eae64838924f1d25350d189e6d672b7c
pworst <pworst@gmail.cm>
2011-05-11 19:29:38 +0200

    Adding missing db4o mapping extenstion classes.

---
+++ Tests/Commons.Persistence.db4o.Tests/Model/Child.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BoC.Persistence;
+
+namespace Commons.Persistence.db4o.Tests.Model
+{
+    public class Child : BaseEntity<long>
+    {
+        public String Name { get; set; }
+    }
+}
