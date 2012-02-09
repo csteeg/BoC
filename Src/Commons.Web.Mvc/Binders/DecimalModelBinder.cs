4c8ec653d5aa4cb1a5d25225618dbb21f01ffbf5
Chris van de Steeg <chris@chrisvandesteeg.nl>
2012-02-09 20:14:10 +0100

    Renaming everything to BoC (except the foldes, perhaps I'll do those also some time soon )

---
+++ Src/Commons.Web.Mvc/Binders/DecimalModelBinder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace BoC.Web.Mvc.Binders
+{
+	public class DecimalModelBinder : DefaultModelBinder
+	{
+		public override object BindModel(
+					ControllerContext controllerContext, 
+					ModelBindingContext bindingContext)
+		{
+			var valueProviderResult = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
+			if (valueProviderResult.AttemptedValue.Equals("N.aN") || valueProviderResult.AttemptedValue.Equals("NaN") || valueProviderResult.AttemptedValue.Equals("Infini.ty") || valueProviderResult.AttemptedValue.Equals("Infinity"))
+				return 0m;
+			return valueProviderResult == null ? base.BindModel(controllerContext, bindingContext) : Convert.ToDecimal(valueProviderResult.AttemptedValue);
+		}
+
+	}
+}
--- Src/Commons/Properties/AssemblyInfo.cs
+++ Src/Commons/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Commons")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("BoC base library")]
+[assembly: AssemblyDescription("Base library for BoC")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Commons")]
-[assembly: AssemblyCopyright("Copyright ©  2008")]
+[assembly: AssemblyCompany("Chris van de Steeg")]
+[assembly: AssemblyProduct("BoC")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 
 [assembly: AssemblyKeyFileAttribute("../../BoC.snk")]
