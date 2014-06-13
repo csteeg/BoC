3153bbe89052d57765527eaeb0156767b7b31df4
Karin Horenberg <Karin.Horenberg@E1289.efocus.local>
2014-06-13 15:52:19 +0200

    bucket unit of work

---
+++ Src/Commons.Persistence.Sitecore/UnitOfWork/SitecoreBucketUnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BoC.Persistence.SitecoreGlass.UnitOfWork
+{
+	public class SitecoreBucketUnitOfWork : SitecoreUnitOfWork
+	{
+		private readonly Item _item;
+
+		private SitecoreBucketUnitOfWork(Item item) : base(null)
+		{
+			_item = item;
+		}
+
+		public static SitecoreBucketUnitOfWork BeginUnitOfWork(Guid itemId, Database db)
+		{
+			return new SitecoreBucketUnitOfWork(db.GetItem(ID.Parse(itemId)));
+		}
+
+		public override IProviderSearchContext IndexSearchContext
+		{
+			get
+			{
+				if (this._index == null)
+					this._index = ContentSearchManager.GetIndex(((SitecoreIndexableItem)_item).CreateSearchContext((SearchSecurityOptions) 2);
+				return this._index;
+			}
+		}
+
+		protected override void CleanUpOuterUnitOfWork()
+		{
+			_item = null;
+			base.CleanUpOuterUnitOfWork();
+		}
+	}
+}
