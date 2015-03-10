8276b2ef8fe6bea7de9b5b8183fb60d79051bed7
Chris van de Steeg <chris.vande.steeg@efocus.nl>
2015-03-10 10:17:04 +0100

    New version

--- Src/Commons.Persistence.Sitecore/Initialize/InitBoc.cs
+++ Src/Commons.Persistence.Sitecore/Initialize/InitBoc.cs
@@ -1,7 +1,5 @@
-﻿using BoC.Persistence.SitecoreGlass.DefaultSetupTasks;
-using BoC.Persistence.SitecoreGlass.Initialize;
+﻿using BoC.Persistence.SitecoreGlass.Initialize;
 using BoC.Web;
-using Sitecore.Pipelines;
 
 [assembly: WebActivatorEx.PostApplicationStartMethod(typeof(InitBoc), "Start")]
 [assembly: WebActivatorEx.PreApplicationStartMethod(typeof(InitBoc), "Disable")]
@@ -16,7 +14,7 @@ namespace BoC.Persistence.SitecoreGlass.Initialize
         }
         public static void Disable()
         {
-            //disable startup init of BoC, need to go after sitecore, so do it in a pipeline
+            //disable startup init of BoC, need to go after sitecore, so do it in a pipeline or in postapplicationstartup if the pipeline version hasn't been triggered
             ApplicationStarterHttpModule.Disabled = true;
         }
     }
