5e865a9baccb7e69ed4dbc48bc9c9776da51936a
Chris van de Steeg <chris.vande.steeg@efocus.nl>
2015-07-06 09:48:13 +0200

    Version update

--- Src/Commons.Persistence.Sitecore/Properties/AssemblyInfo.cs
+++ Src/Commons.Persistence.Sitecore/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BoC.Persistence.Sitecore")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Persistence layer for sitecore using glass.mapper")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BoC.Persistence.Sitecore")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
+[assembly: AssemblyVersion("4.0.2.0")]
+[assembly: AssemblyFileVersion("4.0.2.0")]
