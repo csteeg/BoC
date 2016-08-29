d351287dd0c54a61e7bff3825af24099efecf31d
Chris.vande.Steeg <chris.vande.steeg@efocus.nl>
2016-08-29 11:02:35 +0200

    Update to new glass.mapper

--- Src/Commons.Persistence.Sitecore/Initialize/InitBoc.cs
+++ Src/Commons.Persistence.Sitecore/Initialize/InitBoc.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using BoC.Helpers;
 using BoC.Persistence.SitecoreGlass.Initialize;
 using BoC.Web;
@@ -8,31 +9,44 @@ using BoC.Web;
 namespace BoC.Persistence.SitecoreGlass.Initialize
 {
 
-    public class InitBoc
+  public class InitBoc
+  {
+    public static void Start()
     {
-        public static void Start()
-        {
-            var appdomainhelper = AppDomainHelper.CreateDefault();
-            appdomainhelper.AssemblyFilters.Add(a =>
-                !a.FullName.StartsWith("system.", StringComparison.InvariantCultureIgnoreCase) &&
-                !a.FullName.StartsWith("glimpse.", StringComparison.InvariantCultureIgnoreCase) &&
-                !a.FullName.StartsWith("ironruby.", StringComparison.InvariantCultureIgnoreCase) &&
-                !a.FullName.StartsWith("sitecore.", StringComparison.InvariantCultureIgnoreCase) &&
-                !a.FullName.StartsWith("lucene.", StringComparison.InvariantCultureIgnoreCase) &&
-                !a.FullName.StartsWith("microsoft.", StringComparison.InvariantCultureIgnoreCase) &&
-                !a.FullName.StartsWith("telerik.", StringComparison.InvariantCultureIgnoreCase) &&
-                !a.FullName.StartsWith("webgrease.", StringComparison.InvariantCultureIgnoreCase) &&
-                !a.FullName.StartsWith("yahoo.", StringComparison.InvariantCultureIgnoreCase) &&
-                !a.FullName.StartsWith("stimulsoft.", StringComparison.InvariantCultureIgnoreCase) &&
-                !a.FullName.StartsWith("mongodb.", StringComparison.InvariantCultureIgnoreCase) &&
-                !a.FullName.StartsWith("google.", StringComparison.InvariantCultureIgnoreCase)
-            );
-            BoC.Initializer.Execute(appdomainhelper);
-        }
-        public static void Disable()
-        {
-            //disable startup init of BoC, need to go after sitecore, so do it in a pipeline or in postapplicationstartup if the pipeline version hasn't been triggered
-            ApplicationStarterHttpModule.Disabled = true;
-        }
+      var appdomainhelper = AppDomainHelper.CreateDefault();
+      appdomainhelper.AssemblyFilters.Add(a => !a.FullName.StartsWith("system.", StringComparison.InvariantCultureIgnoreCase) &&
+                                               !a.FullName.StartsWith("glimpse.", StringComparison.InvariantCultureIgnoreCase) &&
+                                               !a.FullName.StartsWith("ironruby.", StringComparison.InvariantCultureIgnoreCase) &&
+                                               !a.FullName.StartsWith("lucene.", StringComparison.InvariantCultureIgnoreCase) &&
+                                               !a.FullName.StartsWith("microsoft.", StringComparison.InvariantCultureIgnoreCase) &&
+                                               !a.FullName.StartsWith("telerik.", StringComparison.InvariantCultureIgnoreCase) &&
+                                               !a.FullName.StartsWith("webgrease.", StringComparison.InvariantCultureIgnoreCase) &&
+                                               !a.FullName.StartsWith("yahoo.", StringComparison.InvariantCultureIgnoreCase) &&
+                                               !a.FullName.StartsWith("stimulsoft.", StringComparison.InvariantCultureIgnoreCase) &&
+                                               !a.FullName.StartsWith("HtmlAgilityPack", StringComparison.InvariantCultureIgnoreCase) &&
+                                               !a.FullName.StartsWith("Lucene.Net", StringComparison.InvariantCultureIgnoreCase) &&
+                                               !a.FullName.StartsWith("RadEditor", StringComparison.InvariantCultureIgnoreCase) &&
+                                               !a.FullName.StartsWith("Stimulsoft", StringComparison.InvariantCultureIgnoreCase) &&
+                                               !a.FullName.StartsWith("ComponentArt.", StringComparison.InvariantCultureIgnoreCase) &&
+                                               !a.FullName.StartsWith("Antlr3.Runtime", StringComparison.InvariantCultureIgnoreCase) &&
+                                               !a.FullName.StartsWith("Newtonsoft.Json", StringComparison.InvariantCultureIgnoreCase) &&
+                                               !a.FullName.StartsWith("google.", StringComparison.InvariantCultureIgnoreCase) &&
+                                               !a.FullName.StartsWith("ucommerce.", StringComparison.InvariantCultureIgnoreCase) &&
+                                               !a.FullName.StartsWith("unicorn.", StringComparison.InvariantCultureIgnoreCase) &&
+                                               !a.FullName.StartsWith("xunit.", StringComparison.InvariantCultureIgnoreCase) &&
+                                               !a.FullName.StartsWith("mongodb.", StringComparison.InvariantCultureIgnoreCase) &&
+                                               (!a.FullName.StartsWith("sitecore.", StringComparison.InvariantCultureIgnoreCase) ||
+                                                a.FullName.StartsWith("sitecore.feature", StringComparison.InvariantCultureIgnoreCase) ||
+                                                a.FullName.StartsWith("sitecore.foundation", StringComparison.InvariantCultureIgnoreCase) ||
+                                                a.FullName.StartsWith("sitecore.common", StringComparison.InvariantCultureIgnoreCase) ||
+                                                a.FullName.StartsWith("sitecore.habitat", StringComparison.InvariantCultureIgnoreCase)
+                                                 ));
+      Initializer.Execute(appdomainhelper);
     }
+    public static void Disable()
+    {
+      //disable startup init of BoC, need to go after sitecore, so do it in a pipeline or in postapplicationstartup if the pipeline version hasn't been triggered
+      ApplicationStarterHttpModule.Disabled = true;
+    }
+  }
 }
